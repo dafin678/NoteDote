b2d177f4ac8f655c9a4be1b94b7e31ed7f3a3ae5
Rizky Juniastiar <rizkyjuniastiar37@gmail.com>
2021-10-28 12:59:37 +0000

    uncomment urls to weakly_schedule

--- weekly_schedule/views.py
+++ weekly_schedule/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-# def index(request):
+def index(request):
+    tes = '<h1>This page is under maintenance</h1>'
+    return HttpResponse(tes)
 
