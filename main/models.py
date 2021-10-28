f05a9bcbde4c6ed543529227dc1e517da93bc35b
Danang Widyorukmantiyoro <danang.widyorukmantiyoro@ui.ac.id>
2021-10-28 21:28:40 +0700

    pushawal

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Motivasi(models.Model):
+    to = models.CharField(max_length=10)
+    pesan = models.CharField(max_length=200)
+    
--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,8 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('motivasi/', views.Add_Motivasi, name='motivasi'),
+    path('view_motivasi/', views.view_motivasi, name='view_motivasi'),
+    path('json/', views.json, name='json'),
+    path('delete_motivasi/<mot_id>/', views.delete_motivasi, name='delete-motivasi'),
 ]
