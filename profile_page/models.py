f8c7ea070449d3ebcdc3ad5d70884355ac39ce40
Josh <josh.steven@ui.ac.id>
2021-10-29 11:38:32 +0700

    Add edit profile form

--- profile_page/models.py
+++ profile_page/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) # Delete profile when user is deleted
     image = models.ImageField(default='profile_pics/default.jpg', upload_to='profile_pics')
     name = models.CharField(max_length=100)
-    about = models.TextField()
-
+    about = models.TextField(default="About Me")
     
+    
+    def save(self, *args, **kwargs):
+        super().save()
+
+        if self.pk is None: # Set default name to username
+            self.name = self.user.username
+        
+        img = Image.open(self.image.path)
+        # Resize image to 250x250
+        if img.height > 250 or img.width > 250:
+            output_size = (250, 250)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
--- profile_page/urls.py
+++ profile_page/urls.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import index
+from .views import index, edit
 
 
 urlpatterns = [
     path('', index, name='index'),
+    path('edit', edit, name='edit'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
