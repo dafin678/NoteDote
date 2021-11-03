2458c3d6fe9f583abf22c9e508d7cbe30952e2c5
Rizky Juniastiar <rizkyjuniastiar37@gmail.com>
2021-11-03 21:57:23 +0700

    Fix error on models.py

--- weekly_schedule/models.py
+++ weekly_schedule/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class weekly_schedule(models.Model):
+class Weekly_schedule(models.Model):
     name = models.CharField(max_length=30)
     day = models.CharField(max_length=15)
-    start_time = models.DateTimeField()
-
+    start_time = models.TimeField()
+    due_time = models.TimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default='')
--- weekly_schedule/views.py
+++ weekly_schedule/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Weekly_schedule
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+# @login_required(login_url='/admin/login')
 def index(request):
-    tes = '<h1>This page is under maintenance</h1>'
-    return HttpResponse(tes)
+    schedules = Weekly_schedule.objects.filter(user=request.user).order_by('start_time')
+    response = {'schedules': schedules}
+    return render(request, 'schedule.html', response)
 
