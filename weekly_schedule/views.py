cdbb3baec3799ab64ad6a13b7cf7ba9edb2e9900
Rizky Juniastiar <rizkyjuniastiar37@gmail.com>
2021-11-04 23:21:26 +0700

    migrate

--- weekly_schedule/views.py
+++ weekly_schedule/views.py
@@ -8,8 +8,8 @@ from .forms import ScheduleForm
 # @login_required(login_url='/admin/login')
 def index(request):
     # profile = UserProfile.objects.create(user=request.user)
-    # schedules = Weekly_schedule.objects.filter(user=request.user).order_by('start_time')
-    schedules = Weekly_schedule.objects.order_by('start_time')
+    schedules = Weekly_schedule.objects.filter(user=request.user).order_by('start_time')
+    # schedules = Weekly_schedule.objects.order_by('start_time')
     response = {'schedules': schedules}
     return render(request, 'schedule.html', response)
 
