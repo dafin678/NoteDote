34089f53214990b66b676c9ba6d3a5dad7e7d613
Josh <josh.steven@ui.ac.id>
2021-11-04 15:00:51 +0700

    add instance model when creating new user

--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Task(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True) # Delete profile when user is deleted
     name = models.CharField(max_length = 150)
     description = models.CharField(max_length = 30)
     task_date = models.DateField()
--- weekly_schedule/signals.py
+++ weekly_schedule/signals.py
@@ -1,7 +1,8 @@
-<<<<<<< HEAD
 # from django.db.models.signals import post_save #Import a post_save signal when a user is created
 # from django.contrib.auth.models import User # Import the built-in User model, which is a sender
-# from django.dispatch import receiver # Import the receiver
+# from django.dispatch import receiver
+
+# from weekly_schedule.models import Weekly_schedule # Import the receiver
 
 # @receiver(post_save, sender=User, dispatch_uid='save_new_user_profile')
 # def save_profile(sender, instance, created, **kwargs):
@@ -9,17 +10,18 @@
 #     if created:
 #         profile = UserProfile(user=user)
 #         profile.save()
-#         # Profile.objects.create(user=instance)
-=======
+#         Weekly_schedule.objects.create(user=instance)
+
 from django.db.models.signals import post_save #Import a post_save signal when a user is created
 from django.contrib.auth.models import User # Import the built-in User model, which is a sender
 from django.dispatch import receiver # Import the receiver
+from .models import Weekly_schedule
 
-@receiver(post_save, sender=User, dispatch_uid='save_new_user_profile')
-def save_profile(sender, instance, created, **kwargs):
-    user = instance
+@receiver(post_save, sender=User) 
+def create_profile(sender, instance, created, **kwargs):
     if created:
-        profile = UserProfile(user=user)
-        profile.save()
-        # Profile.objects.create(user=instance)
->>>>>>> 596c11264019f14a1af6d2cf64f2dd5b61988b9f
+        Weekly_schedule.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
