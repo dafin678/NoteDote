698c59db6009a988cf90a30c8830aad261c8f91f
Muhammad Ilman Nafi'an <muhammadilmannafian@gmail.com>
2021-11-04 06:58:03 +0000

    fix urls

--- notes/urls.py
+++ notes/urls.py
@@ -3,5 +3,5 @@ from .views import add_note, note_list
 
 urlpatterns = [
     path('add-note', add_note, name='add-note'),
-    path('note-list', note_list, name='note-list')
+    path('', note_list, name='note-list')
 ]
