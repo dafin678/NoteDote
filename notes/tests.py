695e66be2082b91772355dc7e33aab5821aefa01
Rizky Juniastiar <rizkyjuniastiar37@gmail.com>
2021-11-05 13:39:18 +0700

    Fix minor error

--- notes/tests.py
+++ notes/tests.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 from django.test import TestCase
 
 # Create your tests here.
-=======
->>>>>>> b1d73f9f6e47f784937506ac803563bf0ec98c3c
-=======
->>>>>>> 244f5dcfc9cadf2afdf0d73ae3b132f75fb2dc99
