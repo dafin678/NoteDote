9ba1533fdd9b99ccd9a6d586d8dbc120b55d212a
nurkhaliff <alief.pamoengkas123@gmail.com>
2021-11-05 20:07:40 +0700

    styling update for login and sign up page

--- login_register/urls.py
+++ login_register/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from.views import login
+from.views import signup
 
 urlpatterns = [
     path('login/', login, name='login'),
-    
+    path('signup/', signup, name='signup')
 ]
