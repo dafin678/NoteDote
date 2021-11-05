e25a21ccf0080f44a8bb5501ceb5300f5e86d383
nurkhaliff <alief.pamoengkas123@gmail.com>
2021-11-05 23:03:13 +0700

    created form for user login and signup

--- login_register/views.py
+++ login_register/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User, auth
+from .forms import CreateUserForm
 
 # Create your views here.
 def login(request):
-    return render(request, 'login_form.html')
+    if request.method=="POST":
+        username=request.POST['username']
+        password=request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/../tasks")
+        else:
+            messages.info(request, 'invalid credentials')
+            return redirect('login')
+    else:
+        return render(request, 'login_form.html')
 
 def signup(request):
-    return render(request, 'signup_form.html')
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form':form}
+    return render(request, 'signup_form.html', context)
