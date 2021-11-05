65007fb59e039b8db1ea83322d7485b82a3a8751
Rizky Juniastiar <rizkyjuniastiar37@gmail.com>
2021-11-05 23:16:25 +0700

    Delete view button

--- weekly_schedule/urls.py
+++ weekly_schedule/urls.py
@@ -6,4 +6,5 @@ app_name = 'weekly_schedule'
 urlpatterns = [
     path('', list_schedule.as_view(), name='list_schedule'),
     path('add-schedule/', add_schedule, name='add_schedule'),
+    # path('delete_schedule/<int:id', delete_schedule, name="delete_schedule"),
 ]
